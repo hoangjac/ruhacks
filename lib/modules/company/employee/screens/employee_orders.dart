41fa211bc65e811de9f087bb526ca2196860bd77
Ifavhoney <nuthsaid@outlook.com>
2020-05-17 10:11:02 -0400

    go

--- lib/modules/company/employee/screens/employee_orders.dart
+++ lib/modules/company/employee/screens/employee_orders.dart
@@ -27,10 +27,11 @@ class EmployeeOrders extends StatelessWidget {
                       itemBuilder: (BuildContext context, int i) {
                         return Padding(
                           padding: EdgeInsets.fromLTRB(85.h, 10.h, 85.w, 10.h),
-                          child: GenericOrdersCard(
+                          child: GenericOrdersAssignedCar(
                             height: 150.h,
                             width: MediaQuery.of(context).size.width,
                             text: "Customer Name",
+                            showAssign: false,
                             isUrgent: true,
                           ),
                         );
@@ -46,7 +47,7 @@ class EmployeeOrders extends StatelessWidget {
                       itemBuilder: (BuildContext context, int i) {
                         return Padding(
                           padding: EdgeInsets.fromLTRB(85.h, 10.h, 85.w, 10.h),
-                          child: GenericOrdersCard(
+                          child: GenericOrdersAssignedCar(
                             height: 150.h,
                             width: MediaQuery.of(context).size.width,
                             text: "Customer Name",
@@ -182,5 +183,115 @@ class EmployeeOrders extends StatelessWidget {
                 ),
               ],
             )));
+  }
+}
+
+class GenericOrdersAssignedCar extends StatelessWidget {
+  final Color color;
+  @required
+  final String text;
+  @required
+  final String serviceName;
+
+  @required
+  final bool isUrgent;
+  final bool showAssign;
+  final double height;
+  final double width;
+  const GenericOrdersAssignedCar({
+    this.color,
+    this.text,
+    this.height,
+    this.width,
+    this.serviceName,
+    this.isUrgent,
+    this.showAssign,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: height + 9.h,
+          width: width,
+          decoration: BoxDecoration(
+            boxShadow: [
+              new BoxShadow(
+                  color: color == null
+                      ? Color(0xff4C5AE8).withOpacity(0.20)
+                      : color,
+                  // offset, the X,Y coordinates to offset the shadow
+                  offset: new Offset(0.0, 1.0),
+                  // blurRadius, the higher the number the more smeared look
+                  blurRadius: 7.0,
+                  spreadRadius: 1.0)
+            ],
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30.h),
+          ),
+        ),
+        Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Display(
+                        color: Theme.of(context).primaryColor,
+                        display: 3,
+                        text: text,
+                        incrementSize: 0.w,
+                      ),
+                      isUrgent == true
+                          ? Padding(
+                              padding: EdgeInsets.fromLTRB(250.w, 9, 0, 0),
+                              child: Align(
+                                  alignment: Alignment.topRight,
+                                  child: Row(
+                                    children: <Widget>[
+                                      Container(
+                                        height: 30.h,
+                                        width: 30.w,
+                                        decoration: BoxDecoration(
+                                          color: Colors.orange,
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 10.w,
+                                      ),
+                                      Display(
+                                        color: Colors.black,
+                                        display: 4,
+                                        text: "URGENT",
+                                        incrementSize: -5.w,
+                                      ),
+                                    ],
+                                  )),
+                            )
+                          : Container(),
+                    ],
+                  ),
+                  subtitle: Display(
+                    display: 4,
+                    color: Color(0xFFC5CEF3),
+                    text: serviceName == null ? "service name" : serviceName,
+                  ),
+                ),
+                SizedBox(
+                  height: 0,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 50.w,
+                  ),
+                ),
+              ],
+            )),
+      ],
+    );
   }
 }
